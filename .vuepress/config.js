543a7a204effea433a54ff39ec44c48b88f8a4b9
half cambodian hacker man <half-kh-hacker@hackery.site>
2019-04-14 22:03:49 +0100

    Initial commit

---
+++ .vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    title: "Paramorphism Documentation",
+    description: "A fast, flexible, and modern obfuscator for Java and Kotlin.",
+
+    themeConfig: {
+        nav: [
+            { text: 'Paramorphism', link: 'https://paramorphism.serenity.enterprises/' }
+        ],
+
+        sidebar: 'auto',
+        sidebarDepth: 4
+    }
+}
